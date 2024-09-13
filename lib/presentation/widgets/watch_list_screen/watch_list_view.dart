bbd8f5e04edeb915e25e46ef7b959814388c4ab1
Melbin <melbinbabu66@gmail.com>
2024-09-13 18:23:16 +0530

    started Implementing search functionality

--- lib/presentation/widgets/watch_list_screen/watch_list_view.dart
+++ lib/presentation/widgets/watch_list_screen/watch_list_view.dart
@@ -27,15 +27,19 @@ class WatchListView extends StatelessWidget {
                       itemBuilder: (context, index) {
                         return StockCard(
                           onPressed: () async {
-                            MostActivelyTradedRemoteDataSourceImpl
-                                mostActivelyTradedRemoteDataSourceImpl =
-                                MostActivelyTradedRemoteDataSourceImpl(
-                                    client: http.Client());
-                            TopGainers gainers =
-                                await mostActivelyTradedRemoteDataSourceImpl
-                                    .fetchMostActivelyTraded();
-                            print('Print from Home screeen view');
-                            print(gainers.mostActivelyTraded.toString());
+                            // MostActivelyTradedRemoteDataSourceImpl
+                            //     mostActivelyTradedRemoteDataSourceImpl =
+                            //     MostActivelyTradedRemoteDataSourceImpl(
+                            //         client: http.Client());
+                            // TopGainers gainers =
+                            //     await mostActivelyTradedRemoteDataSourceImpl
+                            //         .fetchMostActivelyTraded();
+                            // print('Print from Home screeen view');
+                            // print(gainers.mostActivelyTraded.toString());
+                            final url = Uri.parse(
+                                'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=AAPL&apikey=6MYAK98OE63PA571');
+                            final result = await http.get(url);
+                            print(result.body);
                           },
                           stockName: state.watchListData[index].companyName,
                           stockPrice:
